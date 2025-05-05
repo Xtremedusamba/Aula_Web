3b6e04c49dc80d1742a98c4156a1f54a5f7dcd7e
Xtremedusamba <luizfelipeparaiso@gmail.com>
2025-05-04 21:18:22 -0300

    projeto_web

---
+++ PrimeirProjeto/obj/Debug/net9.0/PrimeirProjeto.AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Reflection;
+
+[assembly: System.Reflection.AssemblyCompanyAttribute("PrimeirProjeto")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfccbcaccd1b2ce5857d3e939a1d8ed2165b03ee")]
+[assembly: System.Reflection.AssemblyProductAttribute("PrimeirProjeto")]
+[assembly: System.Reflection.AssemblyTitleAttribute("PrimeirProjeto")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+
+// Gerado pela classe WriteCodeFragment do MSBuild.
+
